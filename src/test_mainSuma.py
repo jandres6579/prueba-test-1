1574cd2e85c98549c3a6560de02b9e0fdf8a1fb0
Juan Andrés <jasanchez@odins.es>
2018-08-17 13:58:11 +0200

    Detectado el error en el Pipeline.

--- src/test_mainSuma.py
+++ src/test_mainSuma.py
@@ -3,6 +3,6 @@ from main import *
 def test_sumaOK():
     assert suma(3,2)==5
 
-
-def test_sumaError():
-    assert suma(3,2)==6
+# Si descomentamos este error el Pipeline falla en todos los puntos posteiores al testeo.
+#def test_sumaError():
+#    assert suma(3,2)==6
