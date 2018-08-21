b0b94f6d9c1f942f24169ae1553a5374685a2d8c
Juan Andrés <jasanchez@odins.es>
2018-08-21 10:15:03 +0200

    Pruebas

--- src/test_mainSuma.py
+++ src/test_mainSuma.py
@@ -12,6 +12,9 @@ def test_sumaOK_prueba1():
 def test_sumaOK_prueba2():
     assert suma(5,7)==12
 
+def test_sumaOK_prueba2():
+    assert suma(5,7)==13
+
 # Si descomentamos este error el Pipeline falla en todos los puntos posteiores al testeo.
 #def test_sumaError():
 #    assert suma(3,2)==6
