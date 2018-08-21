235018cab5b760e70ebfabd11db04e1161ecf673
Juan Andrés <jasanchez@odins.es>
2018-08-21 10:01:49 +0200

    Pruebas lanzar multibranch

--- src/test_mainSuma.py
+++ src/test_mainSuma.py
@@ -6,6 +6,9 @@ def test_sumaOK():
 def test_sumaOK_prueba():
     assert suma(4,2)==6
 
+def test_sumaOK_prueba1():
+    assert suma(5,6)==11
+
 # Si descomentamos este error el Pipeline falla en todos los puntos posteiores al testeo.
 #def test_sumaError():
 #    assert suma(3,2)==6
